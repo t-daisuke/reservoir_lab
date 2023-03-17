15e2fbce97c49ef32e1e60e6d1e03287c1e3988d
t-daisuke <t.daisuke.64@gmail.com>
2023-03-17 21:19:14 +0900

    test completed

--- test.py
+++ test.py
@@ -1,20 +1,7 @@
-import time
+from test_func import *
 
-@profile
-def heavy_loop():
-    for i in range(5000000):
-        loop10()
+def main():
+    print_hello()
 
-    for i in range(10000):
-        loop10()
-
-    for i in range(10):
-        time.sleep(0.1)
-
-    print("executed loop")
-
-def loop10():
-    for i in range(10):
-        pass
-
-heavy_loop()
+if __name__ == '__main__':
+    main()
