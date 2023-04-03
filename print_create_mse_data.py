76db215fc2d10089fdd0124db2c078f8639b1204
t-daisuke <t.daisuke.64@gmail.com>
2023-04-03 16:02:10 +0900

    change print func

--- print_create_mse_data.py
+++ print_create_mse_data.py
@@ -40,10 +40,11 @@ if __name__ == '__main__':
     start_time = time.perf_counter()  # Start
     print("Start Print")
     #create_mse_maps(main_path, saved_test_path, res_params, distance, df, Smesh_list)
-    for i in [1.0, 0.1, 0.01]:
-        res_params = (-9.5, 1, 1000, 0.75, 9, 9,
+    for cone in [0.001, 0.01, 0.1, 1.0]:
+        for sc in [-1, -3, -5, -7]:
+            res_params = (sc, 1, 1000, 0.75, 9, 9,
                   24*60, 3*24*60, 2*24*60-60+1,
-                  1e-8, 2, i)
-        create_mse_maps(main_path, saved_test_path, res_params, distance, df, Smesh_list)
+                  1e-8, 2, cone)
+            create_mse_maps(main_path, saved_test_path, res_params, distance, df, Smesh_list)
     end_time = time.perf_counter()  # End
     print("Saved Print:" + str(end_time - start_time) + "s")
