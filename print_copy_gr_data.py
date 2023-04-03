68a46d9d1a80b9889b95073850c0222aaa927f61
t-daisuke <t.daisuke.64@gmail.com>
2023-04-03 18:15:47 +0900

    copy gr をつくった

---
+++ print_copy_gr_data.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# Ver0324
+
+# import
+import pandas as pd
+import os
+import time
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
+from scipy import linalg
+
+import os
+import time
+
+from print_func import *
+
+if __name__ == '__main__':
+     # Variable
+
+    main_path = './'
+    saved_test_path = './all_prgrm_output/'
+
+    # (expIndex, leakingRate, resSize, spectralRadius, inSize, outSize,
+    #  initLen, trainLen, testLen,
+    #  reg, seed_num, conectivity) = res_params
+    res_params = (-9.5, 1, 1000, 0.75, 9, 9,
+                  24*60, 3*24*60, 2*24*60-60+1,
+                  1e-8, 2, 0.001)
+    distance = 30
+    Smesh_list = [45]
+    center_mesh_mat = (24,35) #ikebukuro 533945774 9*9
+
+    start_time = time.perf_counter()  # Start
+    print("Start Print")
+    for cone in [0.001, 0.01, 0.1, 1.0]:
+        for sc in [-1, -3, -5, -7]:
+            res_params = (sc, 1, 1000, 0.75, 9, 9,
+                  24*60, 3*24*60, 2*24*60-60+1,
+                  1e-8, 2, cone)
+            copy_gr_data(main_path, saved_test_path, res_params, distance, Smesh_list, center_mesh_mat)
+    print("Copy:" + str(time.perf_counter() - start_time) + "s")
--- print_func.py
+++ print_func.py
@@ -140,6 +140,24 @@ def load_mse_map(name, main_path, res_params, distance):
     print("NOT existed")
     return False, 0
 
+#読み込む
+def load_gr_data(mesh_code, main_path, res_params, distance):
+  # write into main_path/gr_data
+  save_path = main_path + "gr_data/"
+  if not os.path.isdir(save_path):
+      os.mkdir(save_path)
+      print("make " + str(save_path))
+  mse_name = save_path + str(res_params[0])
+  for prm_i in range(1,len(res_params)):
+      mse_name += "-" + str(res_params[prm_i])
+  mse_name += "-" + str(distance) + str(mesh_code)
+
+  if os.path.isfile(mse_name+".npz"):
+    return True
+  else:
+    print("NOT existed")
+    return False
+
 def get_mse_map_GR(grl, gmom, main_path, saved_test_path, res_params, distance):
   is_existed, mse = load_mse_map("geo", main_path, res_params, distance)
   if is_existed == True: return mse
@@ -259,4 +277,84 @@ def create_mse_maps(main_path, saved_test_path, res_params, distance, df, Smesh_
   
   diff_map = get_diff_map(Rl, gmom, geo_mse_map, nco_mse_map, main_path, res_params, distance)
   save_or_load_mse_map("diff", main_path, res_params, distance, diff_map)
+  return
+
+def get_copy_gr_data(main_path, saved_test_path, res_params, distance, mesh_list):
+  is_existed = True
+  for m in mesh_list:
+    is_existed = is_existed and load_gr_data(m, main_path, res_params, distance)
+  if is_existed:
+    print("Existed:" + str(mesh_list))
+    print(str(res_params))
+    return
+  
+  save_path = main_path + "gr_data/"
+  if not os.path.isdir(save_path):
+      os.mkdir(save_path)
+      print("make " + str(save_path))
+  
+  for mesh_code in mesh_list:
+    tmp_geo_test_data = load_geo_test_data(saved_test_path, res_params, distance, mesh_code)
+
+    if type(tmp_geo_test_data) != type((1,2)):
+      #ERROE
+      print(tmp_geo_test_data)
+      return tmp_geo_test_data
+    
+    tmp_nco_test_data = load_nco_test_data(saved_test_path, res_params, distance, mesh_code)
+
+    if type(tmp_nco_test_data) != type((1,2)):
+      #ERROE
+      print(tmp_nco_test_data)
+      return tmp_nco_test_data
+    
+    tmp_train_data = load_trained_data(saved_test_path, res_params, mesh_code)
+    if type(tmp_train_data) != type({"A": 1}):
+        print(tmp_train_data)
+        return
+    else:
+      tmp_train_data = tmp_train_data["trained_data"]
+    
+    
+    gr_data_name = save_path + str(res_params[0])
+    for prm_i in range(1,len(res_params)):
+        gr_data_name += "-" + str(res_params[prm_i])
+    gr_data_name += "-" + str(distance) + str(mesh_code)
+    
+    np.savez_compressed(gr_data_name,geo_test_data = tmp_geo_test_data, nco_test_data = tmp_nco_test_data, train_data = tmp_train_data)
+    print("Saved!" + str(gr_data_name))
+    
+
+    
+  print("Copy Data Complete!")
+  return
+
+#default: ikebukuro
+def copy_gr_data(main_path, saved_test_path, res_params, distance, Smesh_list=[], center_mesh_mat = (24,35)):
+  gmom = get_matrix_of_mesh()
+  
+  if len(Smesh_list) != 0:
+    local_area_path = saved_test_path + "smesh"
+    for smesh in Smesh_list:
+        local_area_path += "-" + str(smesh)
+    local_area_path += "/"
+    if not os.path.isdir(local_area_path):
+        print("No path " + str(local_area_path))
+        return
+    saved_test_path = local_area_path
+    
+    main_path = main_path + "smesh"
+    for smesh in Smesh_list:
+        main_path += "-" + str(smesh)
+    main_path += "/"
+    if not os.path.isdir(main_path):
+      os.mkdir(main_path)
+      print("make" + str(main_path))
+  
+  mesh_list=[]
+  for i in [-1, 0, 1]:
+    for j in [-1, 0, 1]:
+      mesh_list.append(gmom["mat"][center_mesh_mat[0] + i, center_mesh_mat[1] + j])
+
+  get_copy_gr_data(main_path, saved_test_path, res_params, distance, mesh_list)
   return
