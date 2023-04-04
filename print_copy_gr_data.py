bbaf7a6e997d4da7172805f6121f72a6b565fea4
t-daisuke <t.daisuke.64@gmail.com>
2023-04-05 02:30:54 +0900

    create mse

--- print_copy_gr_data.py
+++ print_copy_gr_data.py
@@ -24,9 +24,8 @@ if __name__ == '__main__':
     start_time = time.perf_counter()  # Start
     print("Start Print")
     for cone in [0.001, 0.01, 0.1, 1.0]:
-        for sc in [-9]:
-            # [-1, -3, -5, -7]
-            res_params = (sc, 1, 1000, 0.75, 9, 9,
+        for sc in [-1, -3, -5, -7, -9]:
+            res_params = (sc, 1, 100, 0.75, 9, 9,
                   24*60, 3*24*60, 2*24*60-60+1,
                   1e-8, 2, cone)
             copy_gr_data(main_path, saved_test_path, res_params, distance, Smesh_list, center_mesh_mat)
