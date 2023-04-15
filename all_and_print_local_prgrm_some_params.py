84c380e3dba743e768f522c8e0922e834903ae74
t-daisuke <t.daisuke.64@gmail.com>
2023-04-15 13:12:00 +0900

    distanceもいじる

--- all_and_print_local_prgrm_some_params.py
+++ all_and_print_local_prgrm_some_params.py
@@ -30,64 +30,71 @@ if __name__ == '__main__':
                   24*60, 3*24*60, 2*24*60-60+1,
                   1e-8, 2, 1.0)
     distance = 30
-    is_up = True
     Smesh_list = [45]
     center_mesh_mat = (24,35) #ikebukuro 533945774 9*9
     
+    
+    d_list=[1,2,4,8,16,32,64]
+    is_up = False
+    cnct_list = [0.001, 0.01, 0.1, 1.0]
+    sc_list = [-1, -3, -5, -7, -9, -11, -13]
+    
     all_program_start_time = time.perf_counter()
     
-    for cone in [0.001, 0.01, 0.1, 1.0]:
-        for sc in [-10,-11,-12,-13]:
-            # [-1, -3, -5, -7, -9]
-            res_params = (sc, 1, 100, 0.75, 9, 9,
-                  24*60, 3*24*60, 2*24*60-60+1,
-                  1e-8, 2, cone)
-            main_path = './all_prgrm_output/'
-            ###################Reservoir
-            for_time = time.perf_counter()
-            
-            start_time = time.perf_counter() #Start
-            print("Start Train")
+    for d in d_list:       
+        for cone in cnct_list:
+            for sc in sc_list:
+                res_params = (sc, 1, 100, 0.75, 9, 9,
+                    24*60, 3*24*60, 2*24*60-60+1,
+                    1e-8, 2, cone)
+                distance = d
+                
+                main_path = './all_prgrm_output/'
+                ###################Reservoir
+                for_time = time.perf_counter()
+                
+                start_time = time.perf_counter() #Start
+                print("Start Train")
 
-            # create_local_area_trained_data(main_path,res_params,df,Smesh_list,is_update = is_up)
-            #TODO repeat_numはファイル名やres_paramsに含めていません。
-            create_local_area_trained_data_with_repeated_real_data(main_path,res_params,df,Smesh_list,repeat_num=60,is_update = is_up)
+                # create_local_area_trained_data(main_path,res_params,df,Smesh_list,is_update = is_up)
+                #TODO repeat_numはファイル名やres_paramsに含めていません。
+                create_local_area_trained_data_with_repeated_real_data(main_path,res_params,df,Smesh_list,repeat_num=60,is_update = is_up)
 
-            print("Save Train Data:"+ str(time.perf_counter() - start_time) + "s")
+                print("Save Train Data:"+ str(time.perf_counter() - start_time) + "s")
 
-            start_time = time.perf_counter()  # Start
-            print("Start GR")
+                start_time = time.perf_counter()  # Start
+                print("Start GR")
 
-            create_local_gr_test_data(main_path, res_params, distance, df, Smesh_list)
+                create_local_gr_test_data(main_path, res_params, distance, df, Smesh_list)
 
-            print("Save GR Test Data:" + str(time.perf_counter() - start_time) + "s")
+                print("Save GR Test Data:" + str(time.perf_counter() - start_time) + "s")
 
-            start_time = time.perf_counter()  # Start
-            print("Start NCO")
+                start_time = time.perf_counter()  # Start
+                print("Start NCO")
 
-            create_local_nco_test_data(main_path, res_params, distance, df, Smesh_list)
+                create_local_nco_test_data(main_path, res_params, distance, df, Smesh_list)
 
-            print("Save NCO Test Data:" + str(time.perf_counter() - start_time) + "s")
-            
-            print(str(res_params) + "for Time:" + str(time.perf_counter() - for_time) + "s")
-            
-            #####################Print
-            main_path = './'
-            
-            start_time = time.perf_counter()  # Start
-            print("Start mse create")
-            
-            create_mse_maps(main_path, saved_test_path, res_params, distance, df, Smesh_list)
-            
-            print("Save mse create Test Data:" + str(time.perf_counter() - start_time) + "s")
-            
-            start_time = time.perf_counter()  # Start
-            print("Start copy gr data")
-            
-            copy_gr_data(main_path, saved_test_path, res_params, distance, Smesh_list, center_mesh_mat)
-            
-            print("Save copy gr data Test Data:" + str(time.perf_counter() - start_time) + "s")
-            
-            print(str(res_params) + "for Time:" + str(time.perf_counter() - for_time) + "s")
+                print("Save NCO Test Data:" + str(time.perf_counter() - start_time) + "s")
+                
+                print(str(res_params) + "for Time:" + str(time.perf_counter() - for_time) + "s")
+                
+                #####################Print
+                main_path = './'
+                
+                start_time = time.perf_counter()  # Start
+                print("Start mse create")
+                
+                create_mse_maps(main_path, saved_test_path, res_params, distance, df, Smesh_list)
+                
+                print("Save mse create Test Data:" + str(time.perf_counter() - start_time) + "s")
+                
+                start_time = time.perf_counter()  # Start
+                print("Start copy gr data")
+                
+                copy_gr_data(main_path, saved_test_path, res_params, distance, Smesh_list, center_mesh_mat)
+                
+                print("Save copy gr data Test Data:" + str(time.perf_counter() - start_time) + "s")
+                
+                print(str(res_params) + "for Time:" + str(time.perf_counter() - for_time) + "s")
     
     print("All Time:" + str(time.perf_counter() - all_program_start_time) + "s")
