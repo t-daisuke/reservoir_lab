1e1b134918fde2f02b156469622db7bd60fd3851
t-daisuke <t.daisuke.64@gmail.com>
2023-05-01 17:00:27 +0900

    all_prgrmを一つのパラメータ用に改良

--- all_prgrm.py
+++ all_prgrm.py
@@ -8,6 +8,7 @@ import time
 
 from train_func import *
 from test_func import *
+from print_func import *
 
 if __name__ == '__main__':
     # Variable
@@ -23,33 +24,67 @@ if __name__ == '__main__':
     # (expIndex, leakingRate, resSize, spectralRadius, inSize, outSize,
     #  initLen, trainLen, testLen,
     #  reg, seed_num, conectivity) = res_params
-    res_params = (-9.5, 1, 1000, 0.75, 9, 9,
+    res_params = (-9, 1, 100, 0.75, 9, 9,
                   24*60, 3*24*60, 2*24*60-60+1,
                   1e-8, 2, 1.0)
-    distance = 30
+    distance = 32
+    Smesh_list = [45]
     is_up = False
     
-    all_program_start_time = time.perf_counter()
+    # all_program_start_time = time.perf_counter()
+    # start_time = time.perf_counter() #Start
+    # print("Start Train")
+
+    # # create_trained_data_thread(main_path,res_params,df,is_update = True)
+    # create_trained_data(main_path,res_params,df,is_update = True)
+
+    # print("Save Train Data:"+ str(time.perf_counter() - start_time) + "s")
+
+    # start_time = time.perf_counter()  # Start
+    # print("Start GR")
+
+    # create_gr_test_data(main_path, res_params, distance, df)
+
+    # print("Save GR Test Data:" + str(time.perf_counter() - start_time) + "s")
+
+    # start_time = time.perf_counter()  # Start
+    # print("Start NCO")
+
+    # create_nco_test_data(main_path, res_params, distance, df)
+
+    # print("Save NCO Test Data:" + str(time.perf_counter() - start_time) + "s")
+    
+    # print("All Time:" + str(time.perf_counter() - all_program_start_time) + "s")
+    
+    for_time = time.perf_counter()
+    
     start_time = time.perf_counter() #Start
     print("Start Train")
+    
+    create_local_area_trained_data_with_repeated_real_data(main_path,res_params,df,Smesh_list,repeat_num=60,is_update = is_up)
 
-    # create_trained_data_thread(main_path,res_params,df,is_update = True)
-    create_trained_data(main_path,res_params,df,is_update = True)
-
-    print("Save Train Data:"+ str(time.perf_counter() - start_time) + "s")
+    print("Save Train Data:")
+    display_time(time.perf_counter() - start_time)
 
     start_time = time.perf_counter()  # Start
     print("Start GR")
+    print(str(res_params) + "d:" + str(distance)) 
+    print()
 
-    create_gr_test_data(main_path, res_params, distance, df)
+    create_local_gr_test_data(main_path, res_params, distance, df, Smesh_list)
 
-    print("Save GR Test Data:" + str(time.perf_counter() - start_time) + "s")
+    print("Save GR Test Data:" )
+    display_time(time.perf_counter() - start_time)
 
     start_time = time.perf_counter()  # Start
     print("Start NCO")
 
-    create_nco_test_data(main_path, res_params, distance, df)
+    create_local_nco_test_data(main_path, res_params, distance, df, Smesh_list)
 
-    print("Save NCO Test Data:" + str(time.perf_counter() - start_time) + "s")
+    print("Save NCO Test Data:" )
+    display_time(time.perf_counter() - start_time)
     
-    print("All Time:" + str(time.perf_counter() - all_program_start_time) + "s")
+    print("For Time:")
+    display_time(time.perf_counter() - for_time)
+    print(str(res_params) + "d:" + str(distance)) 
+    print()
