e5b20ede2a45c1a4537275342202a727d8665cf7
t-daisuke <t.daisuke.64@gmail.com>
2023-07-12 07:34:26 +0900

    スモールに試す

--- one_step_GR.py
+++ one_step_GR.py
@@ -846,7 +846,7 @@ def smesh_main():
     # sc_list = [-1, -3, -5, -7, -9, -11, -13]
     sc_list = [-1, -3, -5, -7]
     # seed_list=[1,2,3,4,5,6,7,8,9,10]
-    seed_list=[1,2,3,4,5]
+    seed_list=[1,2]
     # seed_list=[2]
     neuro=100
     # neuro = 100
